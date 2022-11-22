38439c6bbbdb14f87f46be6243cc7a4208e6065c
Doyamoo <ryosuke.tanimura+dev@gmail.com>
2022-11-22 21:30:08 +0900

    Pageの作成

--- src/App.js
+++ src/App.js
@@ -1,34 +1,8 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
+import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "じゃけえ",
-  image: "https://source.unsplash.com/RUgbGfdma6U",
-  email: "test@test.com",
-  phone: "080-1111-2222",
-  company: {
-    name: "テスト株式会社",
-  },
-  website: "https://test.com",
-};
-
 function App() {
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>ボタン</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
 
 export default App;
