5d5a3b83389985927ccb4aa9dacf961ba1befc43
Doyamoo <ryosuke.tanimura+dev@gmail.com>
2022-11-22 20:53:21 +0900

    organismsを作成

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import "./App.css";
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
+import "./styles.css";
+
+const user = {
+  name: "じゃけえ",
+  image: "https://source.unsplash.com/RUgbGfdma6U",
+  email: "test@test.com",
+  phone: "080-1111-2222",
+  company: {
+    name: "テスト株式会社",
+  },
+  website: "https://test.com",
+};
 
 function App() {
   return (
@@ -10,6 +22,7 @@ function App() {
       <SecondaryButton>ボタン</SecondaryButton>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
